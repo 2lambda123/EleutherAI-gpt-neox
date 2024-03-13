03b93450065f299b84c934578b9f10f2ac5ad26b
Tobias Fischer <tfischer@nvidia.com>
2024-03-13 12:55:45 +0100

    positive diff of only modified files

--- megatron/core/distributed/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
-
-from .distributed_data_parallel import DistributedDataParallel
-from .finalize_model_grads import finalize_model_grads
-from .grad_buffer import shard_buffer
